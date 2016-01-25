6fb59f3ca22bddefc8765bdd6cb38cd20796d863
Kunal Lad <kunallad423@gmail.com>
2016-01-24 19:34:02 -0600

    Adding call to GetDetailedUserDeck in client

--- client.py
+++ client.py
@@ -8,10 +8,18 @@ def run():
     channel = implementations.insecure_channel('localhost', 50051)
     stub = service_pb2.beta_create_DataRetrieval_stub(channel)
 
-    response = stub.GetDeck("1")
+    request = service_pb2.DeckRequest()
+    request.deckId = "1"
+    response = stub.GetDeck(request)
     print response.deck.SerializeToString
 
-    response = stub.GetUserDeck("1")
+    request = service_pb2.UserDeckRequest()
+    request.userId = "1"
+    request.pageToken = 1
+    response = stub.GetUserDeck(request)
+    print response.deck.SerializeToString
+
+    response = stub.GetDetailedUserDeck(request)
     print response.deck.SerializeToString
 
 
